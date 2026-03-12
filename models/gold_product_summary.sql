7688a2ab93df3abfa71b26b78e93cda66d0d2b5d
MJ-DE17 <manasadevi.j@diggibyte.com>
2026-03-12 20:43:52 +0530

    seed

--- models/gold_product_summary.sql
+++ models/gold_product_summary.sql
@@ -1,8 +1,11 @@
 {{config ( materialized = 'table')}}
 
 SELECT
-    category,
-    COUNT(*) as total_products,
-    AVG(price) as avg_price
-FROM {{ ref('silver_products') }}
-GROUP BY category
+    sp.category,
+    pc.department,
+    COUNT(*) AS total_products,
+    AVG(sp.price) AS avg_price
+FROM {{ ref('silver_products') }} sp
+LEFT JOIN {{ ref('product_category_lookup') }} pc
+ON sp.category = pc.category
+GROUP BY sp.category, pc.department
